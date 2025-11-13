a583f5cd886f643fa6a036a946c8b1ed9e2c1271
lipegol18 <felipecorreati@gmail.com>
2025-11-13 15:37:11 -0300

    upload 13112025

--- server/routes.ts
+++ server/routes.ts
@@ -5,7 +5,7 @@ import { setupAuth, hasPermission, isAuthenticated, checkTrialStatus } from "./a
 import Stripe from "stripe";
 import { WHATSAPP_CONFIG } from "../shared/config";
 
-// Middleware personalizado para relatórios que funciona com autenticação
+// Middleware personalizado para relatórios que funciona com autenticação adadsa
 function reportAuth(req: any, res: any, next: any) {
   console.log("🔍 Verificação de autenticação reportAuth:", {
     isAuthenticated: req.isAuthenticated ? req.isAuthenticated() : false,
