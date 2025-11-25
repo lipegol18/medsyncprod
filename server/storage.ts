63c574dd75005a3cf6617dd392282cc01c1c04db
danielpozzatti <139179147+danielpozzatti@users.noreply.github.com>
2025-11-25 13:05:24 +0000

    Fix typo in HealthInsuranceProvider type import

--- server/storage.ts
+++ server/storage.ts
@@ -1,6 +1,6 @@
 import {
   patients, type Patient, type InsertPatient,
-  healthInsuranceProviders, type Healt\hInsuranceProvider, type InsertHealthInsuranceProvider,
+  healthInsuranceProviders, type HealthInsuranceProvider, type InsertHealthInsuranceProvider,
   healthInsurancePlans, type HealthInsurancePlan, type InsertHealthInsurancePlan,
   opmeItems, type OpmeItem, type InsertOpmeItem,
   opmeSuppliers, type OpmeSupplier, type InsertOpmeSupplier,
