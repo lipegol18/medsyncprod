5bfff888982936be16ba9767e3f750bea4f4c64b
lipegol18 <felipecorreati@gmail.com>
2025-12-08 17:16:26 -0300

    upload 08122025

--- client/src/pages/surgery-appointments.tsx
+++ client/src/pages/surgery-appointments.tsx
@@ -64,7 +64,7 @@ export default function SurgeryAppointments() {
         setPreSelectedOrderId(orderIdInt);
       }
       
-      // Abrir automaticamente o modal de criação
+      // Abrir automaticamente o modal de criação fffffff
       setSelectedAppointment(null);
       setIsDialogOpen(true);
       
